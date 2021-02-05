92aafe6c688c4dde3bae4f62c513dbfc6e547e8b
Ruan Silva <ruanluiz.as@gmail.com>
2021-02-04 21:41:59 -0300

    documentacao em go

---
+++ src/documentacao/doc.go
@@ -0,0 +1,5 @@
+/*
+	Package escrevendodoc ilustra como escrever uma documentação.
+	Isso não tem utilidade nenhuma na prática, é somente exemplo.
+*/
+package escrevendodoc
---
+++ src/documentacao/main.go
@@ -0,0 +1,20 @@
+// Package escrevendodoc ilustra como escrever uma documentação.
+package escrevendodoc
+
+// X é um número inútil
+const x = 10
+
+// Doc é um monte de coisa nenhuma.
+func Doc()  {
+	fmt.Println("Essa função não faz nada.")
+}
+
+// doc2 começa com letra minúscula.
+func doc2()  {
+	fmt.Println("Essa função não faz nada.")
+}
+
+// Doc3 é a última.
+func Doc3()  {
+	fmt.Println("Essa função não faz nada. X é:", x)
+}
