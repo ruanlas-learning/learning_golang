c8da21f5237fc154ed60596863f3e1d7f7399eec
Ruan Silva <ruanluiz.as@gmail.com>
2021-02-05 22:16:46 -0300

    testes como exemplos, benchmarks e cobertura

--- src/testes/main_test.go
+++ src/testes/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"fmt"
 )
 
 type test struct{
@@ -59,16 +60,69 @@ func TestSomaEmTabela(t *testing.T) {
 }
 
 // esse falha
-func TestSomaEmTabela2(t *testing.T) {
+// func TestSomaEmTabela2(t *testing.T) {
+// 	tests := []test{
+// 		test{data: []int{1, 2, 3}, answer: 6},
+// 		test{data: []int{10, 11, 12}, answer: 33},
+// 		test{data: []int{-5, 0, 5, 10}, answer: 12},
+// 	}
+
+// 	for _, v := range tests {
+// 		if res :=soma(v.data...); res != v.answer {
+// 			t.Error("Expected:", v.answer, "Got:", res)
+// 		}
+// 	}
+// }
+
+// teste como exemplo
+func ExampleSoma() {
+	fmt.Println(soma(3, 2, 1))
+	// Output: 6
+}
+
+func ExampleMultiplica() {
+	fmt.Println(multiplica(3, 2, 2))
+	fmt.Println(multiplica(1, 2, 2))
+	fmt.Println(multiplica(2, 2, 2))
+	// Output: 
+	// 12
+	// 4
+	// 8
+}
+
+
+/* 
+	go test testes/ -bench . 
+		* Executa todos os benchmarks
+	go test testes/ -bench Soma
+		* Executa o benchmark apenas da função soma
+*/
+
+func BenchmarkSoma(b *testing.B) {
 	tests := []test{
 		test{data: []int{1, 2, 3}, answer: 6},
 		test{data: []int{10, 11, 12}, answer: 33},
-		test{data: []int{-5, 0, 5, 10}, answer: 12},
+		test{data: []int{-5, 0, 5, 10}, answer: 10},
 	}
 
-	for _, v := range tests {
-		if res :=soma(v.data...); res != v.answer {
-			t.Error("Expected:", v.answer, "Got:", res)
+	for i := 0; i < b.N; i++ {
+		for _, v := range tests {
+			soma(v.data...)
+		}
+	}
+}
+
+
+func BenchmarkMultiplica(b *testing.B) {
+	tests := []test{
+		test{data: []int{1, 2, 3}, answer: 6},
+		test{data: []int{10, 11, 12}, answer: 33},
+		test{data: []int{-5, 0, 5, 10}, answer: 10},
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, v := range tests {
+			multiplica(v.data...)
 		}
 	}
 }
